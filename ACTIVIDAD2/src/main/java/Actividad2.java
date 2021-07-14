f9d277c449df2249ec5cb36391a1ff1ce613f8ed
jona0w0 <jonathanmurillo56@gmail.com>
2021-07-13 18:17:55 -0600

    tarea subir los trabajos a github

---
+++ ACTIVIDAD2/src/main/java/Actividad2.java
@@ -0,0 +1,16 @@
+
+public class Actividad2 {
+public static void main(String[] args) {
+        recurso num1 = new recurso();
+        
+        num1.mensaje();
+        
+        num1.edad();
+        
+        num1.multiplicar();
+        
+        num1.lista_num(); 
+        
+    }
+    
+}
---
+++ ACTIVIDAD2/src/main/java/recurso.java
@@ -0,0 +1,27 @@
+
+public class recurso {
+    public recurso(){
+        System.out.println(" se a creado un objeto ");
+    }
+    int num1=15,num2=7;
+                int parametro =num1*num2;
+    public void mensaje(){
+        System.out.println(" programacion orientada a objetos 2021 ");
+    }
+        public void edad(){
+            int edad=21; 
+            if (edad>= 21){
+            System.out.println(" el estudiante es mayor de edad ");
+            }
+        }
+            public void multiplicar(){
+                
+                System.out.println( "el parametro es: ");
+              System.out.println( parametro);
+            }
+                public void lista_num(){
+                    for(int lista=1; lista<=parametro;lista++){
+                    System.out.println(lista);
+                    }
+                    }
+}
